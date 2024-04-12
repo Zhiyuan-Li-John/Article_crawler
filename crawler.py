79a0d66f2b397e951e92eda64b587a93ffc3e996
Zhiyuan-Li-John <zhli0736@uni.sydney.edu.au>
2024-04-12 17:01:55 +1000

    a little update

--- crawler.py
+++ crawler.py
@@ -1,13 +1,4 @@
-import numpy as np
 from module import arxiv_reader
-import feedparser
-import datetime
-import requests 
-from rank_bm25 import BM25Okapi
-
-def format_date(days=0):
-    return (datetime.datetime.now() - datetime.timedelta(days=days)).strftime('%Y%m%d')
-
 
 if __name__ == "__main__":
     # please add your interest topic word below
